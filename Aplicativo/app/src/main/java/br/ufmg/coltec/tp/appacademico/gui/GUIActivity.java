17c3e46f53baae46d8e7a47287f1d68957599fe3
MariSantos17 <mariana@luarinformatica.com.br>
2018-09-20 10:57:40 -0300

    Tela pronto
    * Começar GUIActivity

---
+++ Aplicativo/app/src/main/java/br/ufmg/coltec/tp/appacademico/gui/GUIActivity.java
@@ -0,0 +1,12 @@
+package br.ufmg.coltec.tp.appacademico.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GUIActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
